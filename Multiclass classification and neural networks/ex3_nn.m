d43fe429555fd9c6da834f951ac356c2340e14a4
avik <avik@ubuntu.ubuntu-domain>
2013-04-07 16:38:12 +0530

    doc changes

--- Multiclass classification and neural networks/ex3_nn.m
+++ Multiclass classification and neural networks/ex3_nn.m
@@ -1,21 +1,3 @@
-%% Machine Learning Online Class - Exercise 3 | Part 2: Neural Networks
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     lrCostFunction.m (logistic regression cost function)
-%     oneVsAll.m
-%     predictOneVsAll.m
-%     predict.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
 %% Initialization
 clear ; close all; clc
 
