1eaee4ed4439b800ee6094e847371626caaec6e5
Avik Pal <avikpal.me@gmail.com>
2013-04-07 16:42:03 +0530

    Update learningCurve.m

--- Regularized Linear Regression/learningCurve.m
+++ Regularized Linear Regression/learningCurve.m
@@ -8,11 +8,6 @@ function [error_train, error_val] = ...
 %       it returns two vectors of the same length - error_train and 
 %       error_val. Then, error_train(i) contains the training error for
 %       i examples (and similarly for error_val(i)).
-%
-%   In this function, you will compute the train and test errors for
-%   dataset sizes from 1 up to m. In practice, when working with larger
-%   datasets, you might want to do this in larger intervals.
-%
 
 % Number of training examples
 m = size(X, 1);
